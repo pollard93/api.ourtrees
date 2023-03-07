fb5aff45dbc29e9413aa876c669b2dd6cf66461a
Piers <ppollard1993@gmail.com>
2023-03-07 14:05:18 +0000

    adds get tree entry comments endpoint

---
+++ __tests__/query/getTreeEntryComments.ts
@@ -0,0 +1,97 @@
+import { gql } from 'graphql-request';
+import '../../global-variables';
+import TestUtils from '../utils';
+import { GetTreeEntryCommentsInput, TreeEntryCommentProfilesPayLoad } from '../../src/resolvers/query/getTreeEntryComments';
+
+
+const query = gql`
+  query getTreeEntryComments($data: GetTreeEntryCommentsInput!){
+    getTreeEntryComments(data: $data){
+      comments {
+        id
+      }
+      count
+    }
+  }
+`;
+
+type Response = { getTreeEntryComments: TreeEntryCommentProfilesPayLoad };
+type Variables = { data?: GetTreeEntryCommentsInput };
+
+
+test('should succeed', async () => {
+  /**
+   * Create user, tree and tree entry
+   */
+  const user = await global.config.utils.createUser();
+  const tree = await global.config.utils.createTree();
+  const treeEntry = await global.config.db.treeEntry.create({
+    data: {
+      notes: TestUtils.randomString(),
+      treeId: tree.id,
+    },
+  });
+
+  /**
+   * Create treeEntry comments
+   */
+  const treeEntryComment1 = await global.config.db.treeEntryComment.create({
+    data: {
+      comment: TestUtils.randomString(),
+      treeEntryId: treeEntry.id,
+      creatorId: user.user.id,
+    },
+  });
+
+  const treeEntryComment2 = await global.config.db.treeEntryComment.create({
+    data: {
+      comment: TestUtils.randomString(),
+      treeEntryId: treeEntry.id,
+      creatorId: user.user.id,
+    },
+  });
+
+  /**
+   * Get
+   */
+  const data = await global.config.client.rawRequest<Response, Variables>(
+    query,
+    {
+      data: {
+        treeEntryId: treeEntry.id,
+        take: 2,
+      },
+    },
+    { authorization: `Bearer ${user.token}` },
+  );
+
+  /**
+   * Test
+   */
+  expect(data.data?.getTreeEntryComments.comments.length).toEqual(2);
+  expect(data.data?.getTreeEntryComments.count).toEqual(2);
+  expect(data.data?.getTreeEntryComments.comments.find((e) => e.id === treeEntryComment1.id)).toBeTruthy();
+  expect(data.data?.getTreeEntryComments.comments.find((e) => e.id === treeEntryComment2.id)).toBeTruthy();
+});
+
+
+test('should fail if tree entry does not exist', async () => {
+  const user = await global.config.utils.createUser();
+
+  try {
+    await global.config.client.rawRequest<Response, Variables>(
+      query,
+      {
+        data: {
+          treeEntryId: 'unknown',
+          take: 2,
+        },
+      },
+      { authorization: `Bearer ${user.token}` },
+    );
+    throw new Error();
+  } catch (error) {
+    // eslint-disable-next-line jest/no-conditional-expect, jest/no-try-expect
+    expect(error.response.errors[0].message).toEqual('Tree entry does not exist');
+  }
+});
---
+++ src/resolvers/query/getTreeEntryComments.ts
@@ -0,0 +1,98 @@
+import 'reflect-metadata';
+import { Resolver,
+  Ctx,
+  Query,
+  UseMiddleware,
+  Int,
+  Field,
+  ObjectType,
+  InputType,
+  Arg,
+  ID } from 'type-graphql';
+import { TreeEntryComment } from '@prisma/client';
+import { Max } from 'class-validator';
+import { TokenType } from '../../modules/Auth/interfaces';
+import { Context } from '../../utils/types';
+import { AuthInterceptor } from '../../modules/Auth/middleware';
+import { TreeEntryCommentProfile } from '../../types/TreeEntryCommentProfile';
+import { GenericError } from '../../errors';
+
+
+export enum SortOrder {
+  asc = 'asc',
+  desc = 'desc',
+}
+
+
+@InputType()
+class TreeEntryCommentWhereUniqueInput {
+  @Field()
+  id: string
+}
+
+
+@InputType()
+export class GetTreeEntryCommentsInput {
+  @Field(() => ID)
+  treeEntryId: string
+
+  @Field(() => TreeEntryCommentWhereUniqueInput, { nullable: true })
+  cursor?: TreeEntryCommentWhereUniqueInput
+
+  @Field()
+  @Max(30)
+  take: number
+}
+
+
+@ObjectType()
+export class TreeEntryCommentProfilesPayLoad {
+  @Field(() => [TreeEntryCommentProfile])
+  comments: TreeEntryCommentProfile[]
+
+  @Field(() => Int)
+  count: number
+}
+
+
+@Resolver()
+export class GetTreeEntryCommentsResolver {
+  @Query(() => TreeEntryCommentProfilesPayLoad)
+  @UseMiddleware(AuthInterceptor({
+    accessTokens: [TokenType.GENERAL],
+  }))
+  async getTreeEntryComments(
+    @Arg('data') data: GetTreeEntryCommentsInput,
+    @Ctx() context: Context<TokenType.GENERAL>,
+  ): Promise<{ comments: TreeEntryComment[], count: number }> {
+    /**
+     * Get comments and return
+     */
+    const comments = await context.db.read.treeEntry.findUnique({ where: { id: data.treeEntryId } }).comments({
+      cursor: data.cursor,
+      take: data.take,
+    });
+
+
+    /**
+     * If treeEntry does not exist
+     */
+    if (comments === null) throw GenericError('Tree entry does not exist');
+
+
+    /**
+     * Count
+     */
+    const count = await context.db.read.treeEntryComment.count({
+      where: {
+        treeEntryId: data.treeEntryId,
+      },
+    });
+
+
+    return {
+      comments,
+      count,
+    };
+  }
+}
